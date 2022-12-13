219cca686c181fff6dcd616a2bc97a09e05299ac
Ciro Gamboa <cirossj10@hotmail.com>
2022-12-12 23:13:43 -0500

    Added EntityExtractor file

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # topic-engine
-MODIFY THIS AND MAKE DAMN ELEGANT
+
+#TODO: Make an introduction of the hackathon context, of Metaculus and the team members
+with proper links
+
+1. Use docker to build and run a local neo4j instance using the following command:
+
+docker run \
+  --name testneo4j \
+  -p7474:7474 -p7687:7687 \
+  -d \
+  -v $HOME/neo4j/data:/data \
+  -v $HOME/neo4j/logs:/logs \
+  -v $HOME/neo4j/import:/var/lib/neo4j/import \
+  -v $HOME/neo4j/plugins:/plugins \
+  --env NEO4J_AUTH=neo4j/test \
+  neo4j:latest
+
+The port 7474 is used for the web interface of the Neo4j client and the 7687 is used for API calls. A default database is created and the root user is neo4j, with password test
+
+
+2. Topic extraction module
+
+3. Merge datasets
+
+4. Create nodes
+
+5. Create relationships
+
+6. Query the data
+
+
---
+++ topic_extraction/EntityExtractor.py
@@ -0,0 +1,152 @@
+import requests
+import dotenv
+import os
+dotenv.load_dotenv()
+
+class EntityExtractor:
+    '''
+    This class contains methods for extracting entities from a given text using Wikifier.
+    Two methods are presented, the first one using the Wikifier API directly
+    and the second one, uses the Wikifier demo capabilities.
+    '''
+    def __init__(self, page_rank=0.006, wiki_data_classes=3):
+        self.headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        self.page_rank = page_rank
+        self.wiki_data_classes = wiki_data_classes
+
+
+    def get_entities_from_api(self,extraction_string,
+                apply_page_rank_sq_threshold,          # to actually discard the annotations whose pagerank is < minPageRank (defaultis False)
+                min_link_freq,                         # default 1
+                max_mention_entropy,                   # cause all highly ambiguous mentions to be ignored float (default -1)
+                max_targets_per_mention,               # to use only the most frequent x candidate annotations for each mention (default x=20)
+                wiki_data_classes,                     # wikiDataClasses (true or false)
+                wiki_data_class_ids):                   # wikiDataClassIds (true or false)
+        
+        '''
+        Extracts the entities from a given text, using a set of tunning parameters for
+        performing a request to the Wikifier API.
+        '''
+
+        user_key = os.environ.get('WIKIFIER_EXTRACTION_KEY')
+
+        api_url = 'http://www.wikifier.org/annotate-article?'
+
+        payload = {'userKey': user_key, 'text': extraction_string, 
+                    'lang': 'en',
+                    'wikiDataClasses': wiki_data_classes,
+                    'wikiDataClassIds':wiki_data_class_ids,
+                    'includeCosines':'false',
+                    'maxMentionEntropy':max_mention_entropy,
+                    'maxTargetsPerMention':max_targets_per_mention,
+                    'minLinkFrequency': min_link_freq,
+                    'applyPageRankSqThreshold': apply_page_rank_sq_threshold,
+                    'pageRankSqThreshold':self.page_rank
+                 }
+
+        try:
+            res = requests.post(api_url, headers=self.headers, data=payload)
+            if (res.status_code == 200):
+                titles = []
+                data = res.json()['annotations']
+
+                filtered_annotations = self.__filter_annotations(data, self.page_rank)
+                return filtered_annotations
+
+            else:
+                raise Exception("Response code: ", res.status_code)
+
+        except Exception as e:
+            print("API call failed: " + str(e))
+            return None
+
+    def get_annotations_from_submit(self,text):
+        '''
+        Extracts the entities from a given text using the demo tool of Wikifier.
+        The demo tool is used emulating an user from the browser.
+        The entities are extracted with the demo predifined parameters and 
+        then filtered by relevance using the page rank.
+        '''
+        data = self.__get_annotated_data(text)
+        annotations = self.__filter_annotations(data)
+        return annotations
+
+    def __get_annotated_data(self,text):
+        '''
+        Performs the request for obtaining the entities to the wikifier demo, using a token
+        for accesing the specific text processing output of the provided text.
+        '''
+        split_token = self.__get_extraction_token(text)
+        data = requests.get('https://wikifier.org/submit.py?cmd=annotate-article&splitToken=' + split_token + '&splitResponse=start')
+        return data.json()
+
+
+    def __get_extraction_token(self,text, language):
+        '''
+        Performs the request to the wikifier demo server for obtaining a token,
+        that is necessary for querying the output of the text processing.
+        '''
+        url = 'https://wikifier.org/submit.py'
+
+        payload = {
+            'languages': language,
+            'secondaryAnnotLanguage': 'en',
+            'text': text,
+            'extraArgs': '',
+            'host': 'posta',
+            'port': 8095,
+            'splitResponse': 'prepare'
+        }
+
+        try:
+            res = requests.post(url, headers=self.headers, data=payload)
+
+            if(res.status_code == 200):
+                res_text = res.text
+                index1 = res_text.find("splitToken") + 14
+                index2 = res_text.find("splitResponse") - 4
+                split_token = res_text[index1:index2]
+
+                return split_token
+            else:
+                raise Exception("Response code: ", res.status_code)
+        
+        except Exception as e:
+            print("Token extraction failed: " + str(e))
+            return None
+        
+
+    def __filter_annotations(self, data):
+        '''
+        The obtained entities are filtered by relevance using the page rank
+        '''
+        annotations = data['annotations']
+        filtered_annotations = []
+
+        for annotation in annotations:
+            # In case there are missing fields
+            try:
+                if(annotation['pageRank'] > self.page_rank):
+                    annot = {}
+                    annot['title'] = annotation['title']
+                    annot['id'] = annotation['wikiDataItemId']
+                    annot['types'], annot['classes'] = self.__filter_types(annotation)
+                    filtered_annotations.append(annot)
+            except Exception as e:
+                print(str(e))
+
+        return filtered_annotations
+
+    def __filter_types(self, annotation):
+        '''
+        The ammount of wiki data classes is limited for avoiding low quality associations
+        with the dbPedia and Wiki Data predefined classes.
+        '''
+        dbpedia_types = annotation['dbPediaTypes']
+        wikidata_classes = annotation['wikiDataClasses'][0:self.wiki_data_classes]
+
+        return dbpedia_types, wikidata_classes
+
